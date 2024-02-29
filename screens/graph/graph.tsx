159a882e6146f8971f012e8fdbd9fb23c1c23b73
Lee <LeeHe@metro.net>
2024-02-28 23:05:13 -0800

    bug fix + screen npms

--- screens/graph/graph.tsx
+++ screens/graph/graph.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import type { PropsWithChildren } from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
   useColorScheme,
   View,
 } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 
